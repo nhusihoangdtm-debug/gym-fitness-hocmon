1b923f8f6591553e52fd807911102e65b32d1af2
nhusihoangdtm-debug <nhusihoangdtm@gmail.com>
2025-10-27 22:37:44 +0700

    add google analytics

--- pages/_documents.js
+++ pages/_documents.js
@@ -4,18 +4,17 @@ export default function Document() {
   return (
     <Html lang="vi">
       <Head>
-        {/* GOOGLE ANALYTICS */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FJQLMYD1QG"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-FJQLMYD1QG', { page_path: window.location.pathname });
-            `,
-          }}
-        />
+        
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-48HYDDBWSJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-48HYDDBWSJ');
+</script>
+
       </Head>
       <body className="bg-black text-white">
         <Main />
